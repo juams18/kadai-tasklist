d97b4e3d58ebfd7d32cd487cdb4cefa51e6dddd3
daichi.takagi <juams18@yahoo.co.jp>
2020-07-19 16:28:46 +0900

    finish

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Task>tasks = em.createNamedQuery("getAllTasks",Task.class)
+                            .getResultList();
+
+        em.close();
+
+        //”tasks”にtasksテーブルを保存することをDBに？命令
+        request.setAttribute("tasks", tasks);
+
+        //rdに画面の遷移先を定義
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/index.jsp");
+
+        //上記で定義した遷移先へ引数requestとresponseを入れて遷移する指示。
+        rd.forward(request,response);
+
+    }
+
+}
